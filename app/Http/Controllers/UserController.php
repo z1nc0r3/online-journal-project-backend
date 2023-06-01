35feaa6e056f5ae766bf001e603d6f0a926fe717
Lasith Manujitha <Manujitha98@gmail.com>
2023-06-01 16:38:07 +0530

    Add Create supervisor logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
         ]);
 
         // Create a new trainee user
-        $user = User::create([
+        User::create([
             'role' => 'trainee',
             'fName' => $request->fName,
             'regNo' => $request->regNo,
@@ -38,6 +38,48 @@ class UserController extends Controller
 
     // Define similar methods for supervisor and evaluator user types
 
-    
+    public function createSupervisor(Request $request) {
+        // Validate the request data
+        $request->validate([
+            'fName' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        // Create a new supervisor user
+        User::create([
+            'role' => 'supervisor',
+            'fName' => $request->fName,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'estName' => $request->estName,
+            'estAddress' => $request->estAddress,
+            'password' => $request->password,
+        ]);
+
+        // Return a success message or any other response as needed
+        return response()->json(['message' => 'Supervisor user created successfully']);
+    }
+
+    public function createEvaluator(Request $request) {
+        // Validate the request data
+        $request->validate([
+            'fName' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        // Create a new evaluator user
+        User::create([
+            'role' => 'evaluator',
+            'fName' => $request->fName,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => $request->password,
+        ]);
+
+        // Return a success message or any other response as needed
+        return response()->json(['message' => 'Evaluator user created successfully']);
+    }
 
 }
