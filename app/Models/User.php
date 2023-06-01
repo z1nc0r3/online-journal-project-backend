0041250ed69f271d32a0dbc306506caa03a70c7c
Lasith Manujitha <Manujitha98@gmail.com>
2023-06-01 13:44:06 +0530

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -18,9 +19,18 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'role',
+        'fName',
+        'regNo',
+        'department',
+        'address',
         'email',
-        'password',
+        'phone',
+        'estName',
+        'estAddress',
+        'startDate',
+        'duration',
+        'password'
     ];
 
     /**
@@ -39,7 +49,11 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
 }
