27b83ea96f3feb09b105fdc95b74637ba47a18c2
Lasith Manujitha <Manujitha98@gmail.com>
2023-06-04 11:28:16 +0530

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,22 @@ Route::group(['prefix' => 'api'], function () {
         Route::post('/supervisor', [UserController::class, 'createSupervisor']);
         Route::post('/evaluator', [UserController::class, 'createEvaluator']);
     });
+
+    Route::group(['prefix' => '/update/'], function () {
+        Route::post('/trainee', [UserController::class, 'updateTrainee']);
+        Route::post('/supervisor', [UserController::class, 'updateSupervisor']);
+        Route::post('/evaluator', [UserController::class, 'updateEvaluator']);
+    });
+
+    Route::group(['prefix' => '/delete/'], function () {
+        Route::post('/trainee', [UserController::class, 'deleteTrainee']);
+        Route::post('/supervisor', [UserController::class, 'deleteSupervisor']);
+        Route::post('/evaluator', [UserController::class, 'deleteEvaluator']);
+    });
+
+    Route::group(['prefix' => '/get/'], function () {
+        Route::get('/trainee/{id}', [UserController::class, 'getTrainee']);
+        Route::get('/supervisor/{id}', [UserController::class, 'getSupervisor']);
+        Route::get('/evaluator/{id}', [UserController::class, 'getEvaluator']);
+    });
 });
